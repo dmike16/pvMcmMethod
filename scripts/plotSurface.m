6828b3e4a910900e903a0963c0a4996b0b6a4def
dmike <cipmiky@gmail.com>
2013-11-04 18:00:20 +0100

    main: update the autogenered function

--- scripts/plotSurface.m
+++ scripts/plotSurface.m
@@ -3,34 +3,34 @@
 clear
 f1 = fopen("/home/dmiky/Documenti/lavori_C_Cpp/tesi/arch/axesNodes.dat");
 for i=1:80
-x(i)=fscanf(f1,"%f",1);
-y(i)=fscanf(f1,"%f",1);
-z(i)=fscanf(f1,"%f",1);
+  x(i)=fscanf(f1,"%f",1);
+  y(i)=fscanf(f1,"%f",1);
+  z(i)=fscanf(f1,"%f",1);
 end
 fclose(f1);
 f2=fopen("/home/dmiky/Documenti/lavori_C_Cpp/tesi/arch/dflMCMsolution.dat");
 f3 = fopen("/home/dmiky/Documenti/lavori_C_Cpp/tesi/arch/IC.dat");
 for i=1:80
-for j=1:80
-for k=1:80
-v(j,k,i)=fscanf(f2,"%f",1);
-u(j,k,i)=fscanf(f3,"%f",1);
-end
-end
+  for j=1:80
+    for k=1:80
+      v(j,k,i)=fscanf(f2,"%f",1);
+      u(j,k,i)=fscanf(f3,"%f",1);
+    end
+  end
 end
 fclose(f2);
 fclose(f3);
 view(-38,20);
 [X,Y,Z]=meshgrid(x,y,z);
 [faces,verts,c]=isosurface(X,Y,Z,u,1,Y);
-figure()
-axis([-4.0,4.0,-4.0,4.0,-4.0,4.0]);
+axis([-4,4,-4,4,-4,4]);
 p = patch("Faces",faces,"Vertices",verts,"FaceVertexCData",c,...
 "FaceColor","interp","EdgeColor","blue");
 set(p,"FaceLighting","phong");
 figure()
-axis([-4.0,4.0,-4.0,4.0,-4.0,4.0]);
+axis([-4,4,-4,4,-4,4]);
 [faces,verts,c]=isosurface(X,Y,Z,v,1,Y);
 p = patch("Faces",faces,"Vertices",verts,"FaceVertexCData",c,...
 "FaceColor","interp","EdgeColor","blue");
 set(p,"FaceLighting","phong");
+pause
